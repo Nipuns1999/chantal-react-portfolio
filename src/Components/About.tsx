2b6eeb7936dd79b2c3f8c0dc691479da6781fd85
Deshan Sumanasinghe <deshan.sumanasinghe@lastyard.com>
2023-04-26 10:01:18 +0800

    Added a pre commit hook to the project

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const About: React.FC = () => {
   return (
     <div className="bg-white py-8">
-    <div className="container mx-auto px-4 flex flex-col items-center">
-      <h2 className="hover:transform hover:scale-110 transition duration-500 ease-in-out font-mono text-3xl font-bold mb-2">About Myself</h2>
-      <img className="hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-full h-60 my-8" src="./pro_pic.jpg"/>
-      <p className="hover:transform hover:scale-110 transition duration-500 ease-in-out font-normal hover:font-semibold text-center font-mono">I started drawing when I was about three years old and over the years, I have learnt various styles and worked with different mediums. I started my Instagram page ‘Paintings by Chantal’ in 2017, and it has been an amazing journey. Celebrating 5 years as an entrepreneur was truly a milestone. So far, I have done over 300 pencil portraits and almost 50 oil paintings.</p>
+      <div className="container mx-auto px-4 flex flex-col items-center">
+        <h2 className="hover:transform hover:scale-110 transition duration-500 ease-in-out font-mono text-3xl font-bold mb-2">
+          About Myself
+        </h2>
+        <img
+          className="hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-full h-60 my-8"
+          src="./pro_pic.jpg"
+        />
+        <p className="hover:transform hover:scale-110 transition duration-500 ease-in-out font-normal hover:font-semibold text-center font-mono">
+          I started drawing when I was about three years old and over the years,
+          I have learnt various styles and worked with different mediums. I
+          started my Instagram page ‘Paintings by Chantal’ in 2017, and it has
+          been an amazing journey. Celebrating 5 years as an entrepreneur was
+          truly a milestone. So far, I have done over 300 pencil portraits and
+          almost 50 oil paintings.
+        </p>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
